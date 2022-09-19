f3c79215940c0340183bf129d5fd344ffcae41fc
juancho1952 <juancho1952@gmail.com>
2022-09-19 17:41:29 -0500

    Agregando un Nuevo trabajo a mi lista de proyectos

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ const lowernavbar = document.querySelector("#navbar")
 bxmenu.addEventListener("click", () => {
     lowernavbar.classList.toggle("navbar-show");
 });
+
+window.addEventListener("load", () => {
+    const loaderTittle = document.querySelector(".loader-tittle")
+    setTimeout(() => {
+        loaderTittle.style.display = "none";
+    }, 2000);
+})
